8225cb7a66e477a1822b8849faa251656f2541f0
ericwafula <ericwathome007@gmail.com>
2022-08-12 16:29:55 +0300

    added test to check for start-time details

--- app/src/test/java/tech/ericwathome/weatherapp/data/remote/repository/WeatherRepositoryImplTest.kt
+++ app/src/test/java/tech/ericwathome/weatherapp/data/remote/repository/WeatherRepositoryImplTest.kt
@@ -1,13 +1,42 @@
 package tech.ericwathome.weatherapp.data.remote.repository
 
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
 import org.junit.Assert.*
 import org.junit.Before
-import tech.ericwathome.weatherapp.data.model.WeatherInfo
+import org.junit.Test
+import tech.ericwathome.weatherapp.data.model.*
 import tech.ericwathome.weatherapp.util.Resource
 
+@ExperimentalCoroutinesApi
 class WeatherRepositoryImplTest: WeatherRepository {
 
+    private lateinit var weatherInfo: WeatherInfo
+
+    @Before
+    fun setup() {
+        weatherInfo = setupWeatherInfoObject()
+    }
+
+    private fun setupWeatherInfoObject(): WeatherInfo {
+        val values = Values(-50.0)
+        val interval = Interval("2022-08-12T03:00:00Z", values)
+        val timeline =
+            Timeline("1d", listOf(interval), "2022-08-12T03:00:00Z", "2022-08-26T03:00:00Z")
+        val data = Data(listOf(timeline))
+        return WeatherInfo(data)
+    }
+
     override suspend fun getCurrentWeatherInfo(lat: Double, long: Double): Resource<WeatherInfo> {
-        TODO("Not yet implemented")
+        return Resource(weatherInfo, null)
+    }
+
+    @Test
+    fun `fetch start-time details, returns true`() {
+        var resource: Resource<WeatherInfo>? = null
+        runTest {
+            resource = getCurrentWeatherInfo(-73.98529171943665, 40.75872069597532)
+        }
+        assertEquals("2022-08-12T03:00:00Z", resource?.data?.data?.timelines?.get(0)?.startTime)
     }
 }
