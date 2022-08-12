f12023dfe5cb5ad4801377e0dd050853ce63a9ab
ericwafula <ericwathome007@gmail.com>
2022-08-12 13:04:40 +0300

    initial di setup and initial dependencies

---
+++ app/src/main/java/tech/ericwathome/weatherapp/di/RepositoryModule.kt
@@ -0,0 +1,10 @@
+package tech.ericwathome.weatherapp.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+class RepositoryModule {
+}
