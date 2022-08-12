3a3390c51ce5e601395c294c71334db7158ca54a
ericwafula <ericwathome007@gmail.com>
2022-08-12 17:02:59 +0300

    added initial viewModel state

--- app/src/main/java/tech/ericwathome/weatherapp/ui/viewmodels/MainActivityViewModel.kt
+++ app/src/main/java/tech/ericwathome/weatherapp/ui/viewmodels/MainActivityViewModel.kt
@@ -1,9 +1,17 @@
 package tech.ericwathome.weatherapp.ui.viewmodels
 
+import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import tech.ericwathome.weatherapp.data.model.WeatherEvent
 import tech.ericwathome.weatherapp.data.remote.repository.WeatherRepository
 import javax.inject.Inject
 
 @HiltViewModel
-class MainActivityViewModel @Inject constructor(repository: WeatherRepository) {
+class MainActivityViewModel @Inject constructor(private val repository: WeatherRepository): ViewModel() {
+    private val _weatherInfo = MutableStateFlow<WeatherEvent>(WeatherEvent.Empty)
+    private val weatherInfo = _weatherInfo.asStateFlow()
+
+
 }
