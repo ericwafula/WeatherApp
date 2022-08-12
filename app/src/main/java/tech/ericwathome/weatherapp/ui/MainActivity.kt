462fa2bd4ff44ca87a4bba8a9d043cdbcda7d28d
ericwafula <ericwathome007@gmail.com>
2022-08-12 14:15:30 +0300

    added WeatherRepositoryImplTest class

--- app/src/main/java/tech/ericwathome/weatherapp/ui/MainActivity.kt
+++ app/src/main/java/tech/ericwathome/weatherapp/ui/MainActivity.kt
@@ -2,8 +2,10 @@ package tech.ericwathome.weatherapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
 import tech.ericwathome.weatherapp.R
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/test/java/tech/ericwathome/weatherapp/data/remote/repository/WeatherRepositoryImplTest.kt
@@ -0,0 +1,9 @@
+package tech.ericwathome.weatherapp.data.remote.repository
+
+import org.junit.Assert.*
+
+class WeatherRepositoryImplTest {
+
+
+
+}
