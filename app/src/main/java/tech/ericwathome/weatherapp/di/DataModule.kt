0348da99ae930eddbfb3bc3144a0b1057c9f4e68
ericwafula <ericwathome007@gmail.com>
2025-05-17 11:37:29 +0300

    added location observer

--- app/src/main/java/tech/ericwathome/weatherapp/di/DataModule.kt
+++ app/src/main/java/tech/ericwathome/weatherapp/di/DataModule.kt
@@ -4,11 +4,14 @@ import io.ktor.client.HttpClient
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
+import tech.ericwathome.core.domain.LocationObserver
 import tech.ericwathome.weatherapp.data.network.HttpClientFactory
 import tech.ericwathome.weatherapp.data.util.DefaultDispatcherProvider
+import tech.ericwathome.weatherapp.datasource.remote.AndroidLocationObserver
 import tech.ericwathome.weatherapp.domain.util.DispatcherProvider
 
 val dataModule = module {
     single<HttpClient> { HttpClientFactory.create() }
     singleOf(::DefaultDispatcherProvider).bind<DispatcherProvider>()
+    singleOf(::AndroidLocationObserver).bind<LocationObserver>()
 }
